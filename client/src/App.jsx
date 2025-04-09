49faa2f58c8af663e94edac58dcf98d4fab67e82
nikhil <nikhillimbu918@gmail.com>
2025-04-09 09:27:05 +0545

    changes added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,27 @@ import { Outlet } from "react-router-dom";
 import { Bounce, ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import { SocketProvider } from "./socketContext";
+import { useDispatch } from "react-redux";
+import { setToken } from "./redux/userSlice";
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      dispatch(setToken(token));
+    }
+  }, [dispatch]);
+
+  axios.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  }, (error) => {
+    return Promise.reject(error);
+  });
   return (
     <>
       <ToastContainer
